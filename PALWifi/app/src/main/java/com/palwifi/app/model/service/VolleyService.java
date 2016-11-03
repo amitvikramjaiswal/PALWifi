fa079dea0e310b2dde3074f94e917f668bdcfb21
amitvikramjaiswal <amitvikramjaiswal@Amits-MacBook-Pro.local>
2016-11-03 21:58:50 +0530

    Volley added

---
+++ PALWifi/app/src/main/java/com/palwifi/app/model/service/VolleyService.java
@@ -0,0 +1,67 @@
+package com.palwifi.app.model.service;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by amitvikramjaiswal on 03/11/16.
+ */
+
+public class VolleyService {
+
+    private static Context mContext;
+    private static VolleyService mVolleyService;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    private VolleyService(Context context) {
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized VolleyService getInstance(Context context) {
+        if (mVolleyService == null) {
+            mVolleyService = new VolleyService(context);
+        }
+        return mVolleyService;
+    }
+
+    private RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
